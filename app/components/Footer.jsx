7d4f321fa8e9ec980715431760d94e2d7e2adf8f
w.Fan <49111639@qq.com>
2016-06-14 16:14:22 +0800

    Revert "app for 0.0.1"
    This reverts commit eec400403f87606dd8bdd4899e7734296f4a58e8.

--- app/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-export default class Footer extends React.Component {
-    render(){
-        return <div className="footer"></div>;
-    }
-}
--- app/utils/requestUrl.js
+++
@@ -1,8 +0,0 @@
-export default function (url) {
-    const APIPATH = '/api/';
-    const APIVERSION = 'v1';
-
-    let apiUrl = APIPATH + APIVERSION + '/' + url;
-
-    return apiUrl;
-};
